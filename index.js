b7b32b08ea5bd42fbe851098a2514acdc7c089d8
nancy sin <nsin@sbcglobal.net>
2017-12-13 03:45:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution in this file.
+function lowerCaseDrivers(names){
+  return names.map(function(drivers){
+    return drivers.toLowerCase();
+  })
+}
+
+function nameToAttributes(names){
+  return names.map(function(name){
+    let fName = name.split(" ")[0]
+    let lName = name.split(" ")[1]
+    return { firstName: fName, lastName: lName}
+  });
+}
+function attributesToPhrase(driver){
+  return driver.map(function(driver)){
+    return '${driver.name} is from ${driver.hometown}';
+  }
+}
